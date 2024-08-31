2ab1ac6f26c229cda35eb3e505ad581272a44f33
Chris Lee <chris@purgo.kr>
2024-08-31 13:52:21 +0900

    Add error handling callback

--- src/Facades/LetsEncrypt.php
+++ src/Facades/LetsEncrypt.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @mixin \Daanra\LaravelLetsEncrypt\LetsEncrypt
- * @method static array create(string $domain, array $chain = [])
+ * @method static array create(string $domain, array $chain = [], callable $catchCallback = null)
  * @method static LetsEncryptCertificate createNow(string $domain)
  * @method static LetsEncryptCertificate renewNow(string|LetsEncryptCertificate $domain)
- * @method static PendingDispatch renew(string|LetsEncryptCertificate $domain, array $chain = [])
+ * @method static PendingDispatch renew(string|LetsEncryptCertificate $domain, array $chain = [], callable $catchCallback = null)
  * @method static PendingCertificate certificate(string $domain)
  */
 class LetsEncrypt extends Facade
